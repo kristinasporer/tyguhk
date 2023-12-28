e2130611915d49c3026b648aaf1cc81fe1e185d0
kristinasporer <155082947+kristinasporer@users.noreply.github.com>
2023-12-28 23:38:41 +0900

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-9c885dfe9b984d99958b8baa0d7f4a8d}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
